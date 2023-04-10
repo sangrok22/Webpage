a37479c9943099d22473ed48cbaa014e399fecbd
sangrok22 <127705529+sangrok22@users.noreply.github.com>
2023-04-10 21:26:51 +0900

    Add files via upload

---
+++ homework342.php
@@ -0,0 +1,10 @@
+<html>
+<html>
+<body>
+
+Width is <?php echo $_GET["width"]; ?><br>
+Height is: <?php echo $_GET["height"]; ?><br>
+직사각형의 면적: <?php echo $_GET["width"]*$_GET["height"]; ?>
+
+</body>
+</html>
